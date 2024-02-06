eb3b7c1c7c31d98719e597498688e726e3ce848f
nhantran3395 <thanhnhan33.95@gmail.com>
2024-02-06 16:35:32 +0700

    feat: set up sign up, login and load todo list after login

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -7,7 +7,9 @@ import { defineAuth } from '@aws-amplify/backend';
  */
 export const auth = defineAuth({
   loginWith: {
-    email: true,
+    email: {
+      verificationEmailSubject: 'Welcome to Notes',
+    },
     // add social providers
     // externalProviders: {
     /**
