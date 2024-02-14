e3d3c0a94c35f0bb7dd954d2eeee779d22b937e3
nhantran3395 <thanhnhan33.95@gmail.com>
2024-02-14 08:30:30 +0700

    style: wrap main with container

--- app/main.tsx
+++ app/main.tsx
@@ -1,6 +1,6 @@
 "use client"
 import { type PropsWithChildren } from "react";
-import { Theme } from '@radix-ui/themes';
+import { Theme, Container } from '@radix-ui/themes';
 
 import { Authenticator } from "@aws-amplify/ui-react";
 import { Button, Flex, Text } from "@radix-ui/themes";
@@ -10,14 +10,18 @@ function Main({ children } : PropsWithChildren) {
         <Authenticator>
             {({signOut, user}) => (
                 <Theme>
-                    <Flex
-                        justify={'end'}
-                        align={'center'}
-                    >
-                        <Text>{user?.username || ''}</Text>
-                        <Button onClick={signOut}>Sign out</Button>
-                    </Flex>
-                    {children}
+                    <Container>
+                        <Flex
+                            justify={'end'}
+                            align={'center'}
+                            gap={'2'}
+                            mt={'2'}
+                        >
+                            <Text>{user?.username || ''}</Text>
+                            <Button onClick={signOut}>Sign out</Button>
+                        </Flex>
+                        {children}
+                    </Container>
                 </Theme>
             )}
         </Authenticator>
